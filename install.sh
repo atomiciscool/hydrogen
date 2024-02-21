fcc800d15a22b9d9e408aec2df5c0d9ee147055e
atomiciscool <146490689+atomiciscool@users.noreply.github.com>
2024-02-21 08:36:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 main() {
-    echo -e "Downloading Latest Roblox - Jords version"
+    echo -e "Downloading Latest Roblox - bomb version"
     [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
     curl "https://setup.rbxcdn.com/mac/version-02b4c0594723475b-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
 
-    echo -e "Installing Latest Roblox - Jords version"
+    echo -e "Installing Latest Roblox - bomb version"
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
     unzip -o -q "./RobloxPlayer.zip"
     mv ./RobloxPlayer.app /Applications/Roblox.app
     rm ./RobloxPlayer.zip
 
-    echo -e "Downloading libHydrogen - Jords version"
+    echo -e "Downloading libHydrogen - bomb version"
     rm ./libHydrogen.dylib
     curl -LJO "https://github.com/JordTheCord/hydrogen/raw/main/libHydrogen.dylib"
 
     rm ./insert_dylib
-    echo -e "Downloading insert_dylib - Jords version"
+    echo -e "Downloading insert_dylib - bomb version"
     curl -LJO "https://github.com/JordTheCord/hydrogen/raw/main/insert_dylib"
 
     chmod +x "./insert_dylib"
