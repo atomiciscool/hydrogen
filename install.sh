98bee7b080d40c20bd6e52a0eae79b9a453524de
atomiciscool <146490689+atomiciscool@users.noreply.github.com>
2024-02-21 08:35:57 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+main() {
+    echo -e "Downloading Latest Roblox - Jords version"
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    curl "https://setup.rbxcdn.com/mac/version-02b4c0594723475b-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+
+    echo -e "Installing Latest Roblox - Jords version"
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+
+    echo -e "Downloading libHydrogen - Jords version"
+    rm ./libHydrogen.dylib
+    curl -LJO "https://github.com/JordTheCord/hydrogen/raw/main/libHydrogen.dylib"
+
+    rm ./insert_dylib
+    echo -e "Downloading insert_dylib - Jords version"
+    curl -LJO "https://github.com/JordTheCord/hydrogen/raw/main/insert_dylib"
+
+    chmod +x "./insert_dylib"
+
+    echo -e "Patching Roblox"
+    mv ./libHydrogen.dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib"
+    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+
+    chmod +x "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib"
+
+    echo -e "Install Complete!"
+}
+
+main
